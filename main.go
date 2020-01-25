13a0d1ac1fb09279af12a16db1fc99aa64cf2c86
Jacob Shore <jacobshore@gmail.com>
2020-01-25 20:13:00 +0200

    fix data retrieval

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	cks "cocktails/cocktails"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -19,6 +21,25 @@ var cocktails Cocktails
 
 // API Route handlers
 func getListOfCocktails(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("sqlite3", "db/cocktails.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	defer db.Close()
+	var results Cocktails
+
+	for _, v := range cocktails {
+		var ingredients []cks.Ingredient
+		var directions []cks.Instruction
+		fmt.Printf("%d", v.ID)
+		db.Where("cocktail_ing_refer = ?", v.ID).Find(&ingredients)
+		db.Where("cocktail_dir_refer = ?", v.ID).Find(&directions)
+		v.Ingredients = append(v.Ingredients, ingredients...)
+		v.Directions = append(v.Directions, directions...)
+		results = append(results, v)
+	}
+	cocktails = results
+	fmt.Println(cocktails)
 	cktls := cks.Cocktails(cocktails).MakeCocktailJSON()
 	w.Header().Set("Content-Type", "application-json")
 	fmt.Fprintf(w, cktls)
@@ -57,43 +78,11 @@ func main() {
 	db.AutoMigrate(&cks.Ingredient{})
 	db.AutoMigrate(&cks.Instruction{})
 	db.AutoMigrate(&cks.Cocktail{})
-
-	db.Create(cks.Cocktail{
-		Name:        "Dry Martini",
-		Description: "Preferred beverage of James Bond",
-		Glass:       "martini glass",
-		Garnish:     "olive or lemon twist",
-		Ingredients: []cks.Ingredient{
-			{Name: "cracked ice", Amount: 2, Unit: "large cubes"},
-			{Name: "London dry gin", Amount: 2.5, Unit: "ounce"},
-			{Name: "dry vermouth", Amount: 0.5, Unit: "ounce"},
-		},
-		Directions: []cks.Instruction{
-			{Step: 1, Instruction: "In mixing glass or cocktail shaker filled with ice, combine gin and vermouth."},
-			{Step: 2, Instruction: "Stir well, about 30 seconds, then strain into martini glass."},
-			{Step: 3, Instruction: "Garnish with olive or lemon twist and serve."},
-		},
-	})
-
-	var cktls cks.Cocktails
-	db.Find(&cktls)
-	var ingredients cks.Ingredient
-	db.Find(&ingredients)
-	db.Model(&cktls).Related(&ingredients)
-	var dm cks.Cocktail
-	db.First(&dm)
-	fmt.Println(dm)
-	// wr.ingredients = append(wr.Ingredients, ingredientsSlice...)
-	fmt.Printf("%v\n", cktls)
-	// db.First(&cktl)
-	// fmt.Printf("%v\n", cktl)
-	// fmt.Println(cks.Cocktails(cktls).MakeCocktailJSON())
-	cks.Cocktails(cktls).Print()
-	// cs := &cocktails
-	cocktails = append(cocktails, cktls...)
-	// fmt.Printf("%T", cs)
-	// router := mux.NewRouter()
-	// router.HandleFunc("/", getListOfCocktails).Methods(http.MethodGet)
-	// router.HandleFunc("/add/", cs.addCocktail).Methods(http.MethodPost)
-	// log.Fatal(http.ListenAndServe(":8080", router))
+	db.Find(&cocktails)
+	cs := &cocktails
+	fmt.Printf("%T", cs)
+	router := mux.NewRouter()
+	router.HandleFunc("/", getListOfCocktails).Methods(http.MethodGet)
+	router.HandleFunc("/add/", cs.addCocktail).Methods(http.MethodPost)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
